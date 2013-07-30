7d9e5c9f8e23005ecebebfba342d7559db230d53
Tom Close <tom.close@cantab.net>
2013-07-30 11:06:53 +0100

    Initial import

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'json'
+
+# TODO create a form with a text area.
+# Write the code to form a word cloud with the text
+# that is pasted into the text area.
+
+# The word_cloud.erb file expects a variable called
+# @words_with_frequencies. This should be a JSON hash.
+#
+# As a simple example of something with the right format:
+#
+# @words_with_frequencies = {'love'=>5, 'coding'=>4}.to_json
+
+get '/' do
+    
+end
